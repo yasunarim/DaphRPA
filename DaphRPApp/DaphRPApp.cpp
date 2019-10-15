90f49e0890e36fae44d95ef5b5ff54d4157f1874
yasunarim <yasunari.matsuo@qdenbs.com>
2019-10-15 22:25:01 +0900

    add app

---
+++ DaphRPApp/DaphRPApp.cpp
@@ -0,0 +1,42 @@
+﻿// DaphRPApp.cpp : アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "framework.h"
+#include "DaphRPApp.h"
+#include "resource.h"
+#include "DaphRPAHook.h"
+
+LRESULT CALLBACK DialogProc(HWND hDlgWnd, UINT msg, WPARAM wp, LPARAM lp);
+
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+						_In_opt_ HINSTANCE hPrevInstance,
+						_In_ LPWSTR    lpCmdLine,
+						_In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	DialogBox(hInstance, MAKEINTRESOURCE(IDD_DAPH_DIALOG), NULL, (DLGPROC)DialogProc);
+	return (int) 1;
+}
+
+LRESULT CALLBACK DialogProc(HWND hDlgWnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+	switch (msg) {
+	case WM_INITDIALOG:
+		return FALSE;
+	case WM_COMMAND:
+		switch (LOWORD(wp)) {
+		case IDC_REC_BTN:
+			StartMouseHook(hDlgWnd);
+			return FALSE;
+		default:
+			return FALSE;
+		}
+
+	default:
+		return FALSE;
+	}
+	return TRUE;
+}
